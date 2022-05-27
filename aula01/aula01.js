050fea0c409e24551cb2aacece9a7f4bd429febb
Wagner Leandro <leandro.loginw@gmail.com>
2022-05-26 22:35:36 -0300

    aula 01 02

---
+++ aula01/aula01.js
@@ -0,0 +1,4 @@
+document.write("CFB Cursos <br>")
+document.writeln(100)
+document.write("<br>50" + 100)
+document.write("<p style = 'color:#f00';>CFB Cursos<br>Curso de JavaScript</p>")
---
+++ aula02/aula02.js
@@ -0,0 +1,15 @@
+let canal = "CFB Cursos";
+let ano = 2017;
+let num = 10;
+
+document.write(canal + "<br>" + ano);
+document.write("<br>---")
+
+let texto = ano + canal;
+
+document.write("<br>" + texto);
+document.write("<br>---")
+
+let texto02 = "<br>" + canal + "<br>" + (ano + num);
+
+document.write(texto02)
