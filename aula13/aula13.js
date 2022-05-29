574e7af911585e21e0a5c158af151a76db1f251c
Wagner Leandro <leandro.loginw@gmail.com>
2022-05-29 15:41:08 -0300

    Aula 13

---
+++ aula13/aula13.js
@@ -0,0 +1,20 @@
+let cor = new Array();
+// OU let cor2 = [];
+
+cor.push("Preto");
+cor.push("Branco");
+cor.push("Vermelho");
+cor.push("Verde");
+cor.push("Azul");
+
+document.write(cor[3] + "<br>");
+
+//
+//
+
+let itens = ["Faca", "Pederneira", "Lanterna", "Luvas", "Chave", "Arame"];
+let mochila = [];
+
+mochila.push(itens[2]);
+
+document.write(mochila[0]);
